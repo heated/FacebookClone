41e4a21295670cea4c065222243bc984950d5438
Edward <edabsw@gmail.com>
2014-01-15 09:31:57 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookClone::Application.config.secret_token = 'cdf81bbfb135fd8f10884dd155177d04516263699d2e2d6b288702a23b2784988eb9dcf3cd2a174e2198aac7d3058ec338e6ac402ea0c07d750270c994bb087c'
