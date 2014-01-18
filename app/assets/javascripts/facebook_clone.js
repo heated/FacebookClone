04111d0414e8854433599ec3c1f6d9ae8e9e25d3
Edward <edabsw@gmail.com>
2014-01-17 18:28:01 -0800

    Helping Julie

--- app/assets/javascripts/facebook_clone.js
+++ app/assets/javascripts/facebook_clone.js
@@ -4,21 +4,9 @@ window.FacebookClone = {
   Views: {},
   Routers: {},
   initialize: function() {
-    this.users = new FacebookClone.Collections.Users();
-    this.messages = new FacebookClone.Collections.Messages();
-    this.posts = new FacebookClone.Collections.Posts();
-
-    var usersFetch = this.users.fetch();
-    var messagesFetch = this.messages.fetch();
-    var postsFetch = this.posts.fetch();
-
-    $.when(
-      usersFetch, messagesFetch, postsFetch
-    ).then(function() {
-      new FacebookClone.Routers.MainRouter({
-        $rootEl: $('#content')
-      });
-      Backbone.history.start();
+    new FacebookClone.Routers.MainRouter({
+      $rootEl: $('#content')
     });
+    Backbone.history.start();
   }
 };
