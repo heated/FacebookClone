4792d2820c2ac621008e98bc07c89090a7e124f3
Edward <edabsw@gmail.com>
2014-01-18 11:36:18 -0800

    Cleaned files.

--- app/assets/javascripts/models/comment.js
+++ app/assets/javascripts/models/comment.js
@@ -1,13 +1,8 @@
 FacebookClone.Models.Comment = Backbone.Model.extend({
   urlRoot: "comments",
 
-  initialize: function(data) {
-    this.parse(data);
-  },
-
   parse: function(data) {
     data.user = new FacebookClone.Models.User(data.user);
-    console.log(data.user);
     return data;
   }
 });
