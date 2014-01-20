9c8652f7568a882dc2e99766e096efefebcddb94
Edward <edabsw@gmail.com>
2014-01-19 18:10:24 -0800

    Fixing up Rails views for authentication.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(params[:user])
     if @user.save
       log_in_user(@user)
-      redirect_to user_url(@user)
+      redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
